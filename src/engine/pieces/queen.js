2ce432c5ba502ef15c71236844c1f508777c5ddd
Fuad Ashanike <Fuad.Ashanike@softwire.com>
2017-11-15 15:23:14 +0000

    stop movement if there is a piece in front of a queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -7,8 +7,9 @@ export default class Queen extends Piece {
     }
 
     getCandidateMoves(board) {
-        return [0,1,2,3,4,5,6,7].map(row => [0,1,2,3,4,5,6,7].map(col => new Square(row, col))).reduce((acc, x) => acc.concat(x))
-            .filter(square => Math.abs(square.row - this.currentSquare.row) === Math.abs(square.col - this.currentSquare.col) || square.row === this.currentSquare.row || square.col === this.currentSquare.col)
-            .filter(square => !(square.row === this.currentSquare.row && square.col === this.currentSquare.col));
+        return Piece.getBRQMoves(board, this.currentSquare, [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]);
+        // return [0,1,2,3,4,5,6,7].map(row => [0,1,2,3,4,5,6,7].map(col => new Square(row, col))).reduce((acc, x) => acc.concat(x))
+        //     .filter(square => Math.abs(square.row - this.currentSquare.row) === Math.abs(square.col - this.currentSquare.col) || square.row === this.currentSquare.row || square.col === this.currentSquare.col)
+        //     .filter(square => !(square.row === this.currentSquare.row && square.col === this.currentSquare.col));
     }
 }
