1fc482fa456eeb7d4117afd76e2aa643f3b94291
Fuad Ashanike <Fuad.Ashanike@softwire.com>
2017-11-15 16:19:22 +0000

    king now takes pieces

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -6,9 +6,10 @@ export default class King extends Piece {
         super(player);
     }
 
-    getCandidateMoves(board) {
-        return [0,1,2,3,4,5,6,7].map(row => [0,1,2,3,4,5,6,7].map(col => new Square(row, col))).reduce((acc, x) => acc.concat(x))
-            .filter(square => (square.row - this.currentSquare.row) ** 2 + (square.col - this.currentSquare.col) ** 2 < 3)
-            .filter(square => !(square.row === this.currentSquare.row && square.col === this.currentSquare.col));
+    getAvailableMoves(board) {
+        return Piece.getKMoves(board, this.player, this.currentSquare, [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, -1], [-1, 1]]);
+        // return [0,1,2,3,4,5,6,7].map(row => [0,1,2,3,4,5,6,7].map(col => new Square(row, col))).reduce((acc, x) => acc.concat(x))
+        //     .filter(square => (square.row - this.currentSquare.row) ** 2 + (square.col - this.currentSquare.col) ** 2 < 3)
+        //     .filter(square => !(square.row === this.currentSquare.row && square.col === this.currentSquare.col));
     }
 }
