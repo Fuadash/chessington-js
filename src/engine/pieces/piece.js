38fc1b41da5b1743434769e07b685d4c679333ae
Fuad Ashanike <Fuad.Ashanike@softwire.com>
2017-11-15 15:22:29 +0000

    stop movement if there is a piece in front of a bishop

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,3 +1,5 @@
+import Square from '../square';
+
 export default class Piece {
     constructor(player) {
         this.player = player;
@@ -11,6 +13,22 @@ export default class Piece {
 
     getCandidateMoves(board) {
         throw new Error('This method must be implemented, and return a list of available moves');
+    }
+
+    static getBRQMoves(board, originSquare, directions){
+        let candidates = [];
+        directions.forEach(direction => {
+            let change = [direction[0], direction[1]];
+            while (true) {
+                let checkSquare = originSquare.shiftedBy(change[0], change[1]);
+                if (!board.squareIsOnBoard(checkSquare)) break;
+                if (board.getPiece(checkSquare)) break;
+                candidates.push(checkSquare);
+                change[0] += direction[0];
+                change[1] += direction[1];
+            }
+        });
+        return candidates;
     }
 
     moveTo(board, newSquare) {
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -7,21 +7,10 @@ export default class Rook extends Piece {
     }
 
     getCandidateMoves(board) {
-        let candidates = [];
-        let directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-        directions.forEach(direction => {
-            let change = [direction[0], direction[1]];
-            while (true) {
-                let checkSquare = this.currentSquare.shiftedBy(change[0], change[1]);
-                if (!board.squareIsOnBoard(checkSquare)) break;
-                if (board.getPiece(checkSquare)) break;
-                candidates.push(checkSquare);
-                change[0] += direction[0];
-                change[1] += direction[1];
-            }
-        });
-        console.log(candidates);
-        return candidates;
+        return Piece.getBRQMoves(board, this.currentSquare, [[1, 0], [0, 1], [-1, 0], [0, -1]]);
+    }
+
+    getLongFunction(board, directions) {
         // return [0,1,2,3,4,5,6,7].filter(x => x !== this.currentSquare.col).map(x => new Square(this.currentSquare.row, x)).concat(
         //     [0,1,2,3,4,5,6,7].filter(x => x !== this.currentSquare.row).map(x => new Square(x, this.currentSquare.col))
         // );
