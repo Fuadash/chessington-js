a9ea0f4f73792e3a6ae4357d5c2965f70b7c04f6
Fuad Ashanike <Fuad.Ashanike@softwire.com>
2017-11-15 12:22:44 +0000

    knight movement

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,7 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        return [0,1,2,3,4,5,6,7].map(row => [0,1,2,3,4,5,6,7].map(col => new Square(row, col))).reduce((acc, x) => acc.concat(x))
+            .filter(square => (square.row - this.currentSquare.row) ** 2 + (square.col - this.currentSquare.col) ** 2 === 5);
     }
 }
